38c3ae6d0f395d5cc2dccd0060299a699e2ab6b5
PTC\odsptc15\AKomar <vovik_82008@ukr.net>
2017-03-16 17:58:34 +0200

    16_03

---
+++ Algorithms/src/BinarySearch.java
@@ -0,0 +1,26 @@
+/**
+ * Created by odsptc15 on 3/16/2017.
+ */
+public class BinarySearch {
+
+    public static int search(int[]data, int key)
+    {
+        int beginIndex = 0;
+        int endIndex = data.length-1;
+
+        int mediana = (beginIndex + endIndex) /2;
+
+        //searchHelper(beginIndex, mediana);
+        //searchHelper(mediana, endIndex);
+
+        return -1;
+    }
+
+    private int searchHelper(int begin, int end)
+    {
+        //if(begin == end)
+            return -1;
+
+    }
+
+}
--- Algorithms/src/Launcher.java
+++ Algorithms/src/Launcher.java
@@ -4,52 +4,48 @@
 public class Launcher {
     public static void main(String []args)
     {
-        int[]testData = {1, 19, 45, 5, 89, 10};
-
-
-
-        Expression expression = NaiveSearcher::naiveSearch;
+        Expression naiveSearch = NaiveSearcher::naiveSearch;
 
         //key not exists in array
         int searchKey = 1;
 
         //degerate
-        Tester.test(-1,expression,new int[]{},searchKey);
+        Tester.test(-1,naiveSearch,new int[]{},searchKey);
 
         //trivial
-        Tester.test(-1,expression, new int[]{2},searchKey);
+        Tester.test(-1,naiveSearch, new int[]{2},searchKey);
 
         //trivial-2
-        Tester.test(-1,expression,new int[]{2,8},searchKey);
+        Tester.test(-1,naiveSearch,new int[]{2,8},searchKey);
 
         //general
-        Tester.test(-1,expression,new int[]{2,8,5,7,9},searchKey);
+        Tester.test(-1,naiveSearch,new int[]{2,8,5,7,9},searchKey);
 
         //general
-        Tester.test(-1,expression,new int[]{9,10,11,12},searchKey);
+        Tester.test(-1,naiveSearch,new int[]{9,10,11,12},searchKey);
 
         //general
-        Tester.test(-1,expression,new int[]{4,5,7,2},searchKey);
+        Tester.test(-1,naiveSearch,new int[]{4,5,7,2},searchKey);
 
 
         //key exists in array
 
         //degerate
-        Tester.test(-1,expression,new int[]{},searchKey);
+        Tester.test(-1,naiveSearch,new int[]{},searchKey);
 
         //trivial
-        Tester.test(0,expression,new int[]{searchKey},searchKey);
+        Tester.test(0,naiveSearch,new int[]{searchKey},searchKey);
 
         //trivial-2
-        Tester.test(1,expression,new int[]{2,searchKey},searchKey);
+        Tester.test(1,naiveSearch,new int[]{2,searchKey},searchKey);
 
         //general
-        Tester.test(3,expression,new int[]{2,8,5,searchKey,7,9},searchKey);
+        Tester.test(3,naiveSearch,new int[]{2,8,5,searchKey,7,9},searchKey);
 
         //general
-        Tester.test(0,expression,new int[]{searchKey,9,10,11,12},searchKey);
+        Tester.test(0,naiveSearch,new int[]{searchKey,9,10,11,12},searchKey);
 
         //general
-        Tester.test(4,expression,new int[]{4,5,7,2,searchKey},searchKey);
+        Tester.test(4,naiveSearch,new int[]{4,5,7,2,searchKey},searchKey);
     }
 }
